23e8007fcec0a365d37ef1958a3ef1db325217d0
jasonvandewarker <jasonvandewarker@gmail.com>
2017-10-17 17:06:37 -0700

    Added a few more small things.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,41 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Mega Essential Links</h2>
+    <h2>Mega Essential Stuff</h2>
     <ul>
       <div>
         <ul>
         <li>These guys were totally in the Beatles:</li>
-      <li v-for="user in users">
+          <li v-for="user in users">
         {{user.firstName}} {{user.lastName}}
         </li>
         </ul>
         </div>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+
+        <div>
+          <input type="text" v-model="input_val">
+        </div>
+
+        <div>
+          Input Value: <span v-text="input_val"></span>
+
+
+        </div>
+      
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
     <h2>Stuff</h2>
     <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
       <li><a href="http://github.com/jasonvandewarker" target="_blank">Git Hub</a></li>
-      
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'hello',
   data () {
     return {
       msg: 'This is from within the data object. I added the members of the Beatles for no reason.',
@@ -36,9 +43,9 @@ export default {
         {firstName: 'Paul', lastName: 'McCartney'},
         {firstName: 'John', lastName: 'Lennon'},
         {firstName: 'George', lastName: 'Harrison'},
-        {firstName: 'Ringo', lastName: 'Starr'}
-
-      ]
+        {firstName: 'Ringo', lastName: 'Starr'},
+      ],
+      input_val: '',
     }
   }
 }
